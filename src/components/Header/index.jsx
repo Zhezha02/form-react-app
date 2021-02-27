abee00ec2dd7785194486f7933c9fa6e2d7ea6bc
Evgeniya <evgeniyakirtoka@gmail.com>
2021-02-27 17:59:41 +0200

    Add Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.scss';
+import logo from './logo.png';
+
+const Header = props => {
+  const {
+    link: { path, description },
+  } = props;
+  return (
+    <div className={styles.container}>
+      <Link to='/'>
+        <img src={logo} alt='logo' />
+      </Link>
+      
+      <Link to={path} className={styles.link}>
+        {description}
+      </Link>
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  link: { path: '/', description: 'Home' },
+};
+Header.propTypes = {
+  link: PropTypes.objectOf(PropTypes.string),
+};
+
+export default Header;
