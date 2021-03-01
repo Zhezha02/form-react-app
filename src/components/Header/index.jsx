5ad6c1ad1581594e519b3ee77d5d4767616c18da
Evgeniya Kirtoka <evgeniyakirtoka@gmail.com>
2021-03-01 20:08:31 +0200

    Move logo img into public

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,18 +3,17 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import cx from 'classnames';
-import logo from './logo.png';
 
 const Header = props => {
   const {
     link: { path, description },
   } = props;
   return (
-    <div className={cx(styles.container) }>
+    <div className={cx(styles.container)}>
       <Link to='/'>
-        <img src={logo} alt='logo' />
+        <img src='/static/images/logo.png' alt='logo' />
       </Link>
-      
+
       <Link to={path} className={styles.link}>
         {description}
       </Link>
